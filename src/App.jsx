c8601c441d66c2d766a2fe1a887dafb41239dd40
Somdotta07 <jhilick.s@gmail.com>
2022-05-17 02:58:03 +0200

    Set up with React and added components, basic styles

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Projects from './components/project/Projects'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
+
+    </>
+  )
+}
+
+export default App
