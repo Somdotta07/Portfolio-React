708911a17e2b579646b9924b63ef4ae1b8789fd8
Somdotta07 <jhilick.s@gmail.com>
2022-05-23 16:30:47 +0200

    Add projects and testimonial components

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
-import './project.css'
+import React from "react";
+import "./project.css";
+import IMG1 from '../../assets/somarven.png';
 
+
+const projects = [{
+  id: 1,
+  image: IMG1,
+  title: 'Somarven full-stack project',
+  github: 'https://github.com/Somdotta07/Somarven-Arenas-frontend',
+  demo:'https://somarven.netlify.app/'
+},
+ {
+  id: 2,
+  image: IMG1,
+  title: 'Somarven full-stack project',
+  github: 'https://github.com/Somdotta07/Somarven-Arenas-frontend',
+  demo:'https://somarven.netlify.app/'
+  }, 
+ {
+  id: 3,
+  image: IMG1,
+  title: 'Somarven full-stack project',
+  github: 'https://github.com/Somdotta07/Somarven-Arenas-frontend',
+  demo:'https://somarven.netlify.app/'
+  },
+ {
+  id: 4,
+  image: IMG1,
+  title: 'Somarven full-stack project',
+  github: 'https://github.com/Somdotta07/Somarven-Arenas-frontend',
+  demo:'https://somarven.netlify.app/'
+  },
+ {
+  id: 5,
+  image: IMG1,
+  title: 'Somarven full-stack project',
+  github: 'https://github.com/Somdotta07/Somarven-Arenas-frontend',
+  demo:'https://somarven.netlify.app/'
+  },
+ {
+  id: 6,
+  image: IMG1,
+  title: 'Somarven full-stack project',
+  github: 'https://github.com/Somdotta07/Somarven-Arenas-frontend',
+  demo:'https://somarven.netlify.app/'
+},
+]
 const Projects = () => {
   return (
-     <section id='project'>Project</section>
-  )
-}
+    <section id="project">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container project_container">
+        {projects.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="project_item">
+          <div className="project_item-image">
+          <img src={image} alt={title} />
+          </div>
+              <h3>{title}</h3>
+          <div className="project-item-cta">
+                     <a
+            href={github}
+            className="btn"
+          >
+            Github
+          </a>
+          <a href={demo} className="btn btn-primary" target='_blank' rel="noreferrer">
+            Live Demo
+          </a>
+          </div> 
+        </article>
+          )
+        })}               
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar3.jpg";
+
+const peers = [
+  {
+    avatar: AVTR1,
+    name: "Rachel Higgins",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Rerum molestiae, corporis possimus consectetur consequuntur eaque dignissimos in reiciendis corrupti necessitatibus ea laborum odit  voluptate nobis quas, explicabo quisquam velit modi!",
+  },
+  {
+    avatar: AVTR2,
+    name: "Jim Scott",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Rerum molestiae, corporis possimus consectetur consequuntur eaque dignissimos in reiciendis corrupti necessitatibus ea laborum odit  voluptate nobis quas, explicabo quisquam velit modi!",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from Peers</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonials_container">
+        {peers.map(({ avatar, name, review }, index) => {
+          return (
+            <article key={index} className="testimonial">
+              <div className="peer_avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="peer_name">{name}</h5>
+              <small className="peer_review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
