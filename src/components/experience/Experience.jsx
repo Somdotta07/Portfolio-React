8712507f835e4f44af741e4f189cb7d53ef77f16
Somdotta07 <jhilick.s@gmail.com>
2022-05-23 21:50:18 +0200

    Add contact form and options but the emailjs is throwing error, need to check later

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
     <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
-      <div className=" container experience_container">
+      <div className="container experience_container">
         <div className="experience_frontend">
           <h3>Frontend Development</h3>
           <div className="experience_content">
